25065ef46003e086d87297fffc9c46129d873fe8
Claudio <claudio.pinho@blockbastards.io>
2019-09-03 01:27:50 +0100

    Organization

--- Debug/Adafruit_GFX_Library/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\claud\AppData\Local\Temp\VMBuilds\Console_esp32_128x128\esp32_esp32\Debug\Adafruit_GFX_Library\glcdfont.c.o: \
- C:\Users\claud\Documents\Arduino\libraries\Adafruit_GFX_Library\glcdfont.c
